77de4a14d8dd4e3c34ef823df8e0af709f3c4452
Andrew Roth <a.roth.80@hotmail.com>
2024-11-30 18:15:38 -0800

    .get for urlDatabase added

--- express_server.js
+++ express_server.js
@@ -7,8 +7,12 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.length("/", (req, res) => {
+app.get("/", (req, res) => {
   res.send("Hello!");
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
 });
 
 app.listen(PORT, () => {
